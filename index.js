6bc16cd93f5ed1d662ad2da920ce236a97548070
Jonatas Passos <jpc080496@gmail.com>
2022-05-05 15:53:22 -0300

    testando

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res
-  .send(`<h1> Executando na porta: ${port} bora </h1>`));
+  .send(`<h1> Executando na porta: ${port} testandoo </h1>`));
 
 app.listen(port, () => console.log(`Online: ${port}`));

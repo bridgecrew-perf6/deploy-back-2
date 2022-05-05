f7135bc18e79d485a5d30f06958fdb753ce1393e
Jonatas Passos <jpc080496@gmail.com>
2022-05-05 15:17:24 -0300

    meu primeiro commit na plataforma heroku com Docker

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} bora </h1>`));
+app.get('/', (req, res) => res
+  .send(`<h1> Executando na porta: ${port} bora </h1>`));
+
 app.listen(port, () => console.log(`Online: ${port}`));
